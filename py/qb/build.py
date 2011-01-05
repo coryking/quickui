320e41662d71fee3e7cad408f11fa84ac58ee56f
Jan Miksovsky <jan@miksovsky.com>
2011-01-04 22:21:43 -0800

    Tinkering with how to factor build process

---
+++ py/qb/build.py
@@ -0,0 +1,11 @@
+class Build(object):
+    '''
+    Compiles multiple QuickUI markup files in a project.
+    '''
+    
+    def __init__(self, build_path):
+        # do the compile right away?
+        # record success, js files, css files as member variables?
+
+    def compile(self):
+        pass
--- py/qb/build_unit.py
+++ py/qb/build_unit.py
@@ -1,7 +1,19 @@
+import os
+from project import Project
+
 class BuildUnit(object):
     '''
     Relates a QuickUI source file with its correspdonding generated JS and CSS output.
     '''
+    
     def __init__(self, path_qui, path_build):
-        # os.path.splitext(os.path.basename(foo))
-        pass
+        basename = os.path.splitext(os.path.basename(path_qui))[0]
+        self.filename_js = basename + Project.FILE_EXTENSION_JS
+        self.filename_css = basename + Project.FILE_EXTENSION_CSS
+
+import unittest
+class BuildUnitTests(unittest.TestCase):
+    def test_names(self):
+        unit = BuildUnit("/project/Foo.qui", "/project/build")
+        self.assertEqual("Foo.js", unit.filename_js)
+        self.assertEqual("Foo.css", unit.filename_css)
